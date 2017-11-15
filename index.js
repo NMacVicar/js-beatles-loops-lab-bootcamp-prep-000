09440046396718945648dda4e6e02a0c66604606
Neil MacVicar <n.macvicar9@gmail.com>
2017-11-15 18:20:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
-  var array = []
-
+  var playing = []
+  for (var i = 0; i < musicians.length; i++){
+    playing.push(`${musicians[i]} plays the ${instruments[i]}`)
+  }
+  return playing
 }
